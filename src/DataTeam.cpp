8f667d49c166bf6b5450cf573d997c9df53d5f1b
francarfox <francarfox@gmail.com>
2015-11-15 00:16:36 -0300

    #1 Lectura de train.csv funcionando

--- src/DataTeam.cpp
+++ src/DataTeam.cpp
@@ -7,7 +7,9 @@
 //============================================================================
 
 #include <iostream>
+#include <fstream>
 #include <stdio.h>
+#include <list>
 using namespace std;
 
 //	Debe eliminar descripcion y resolucion
@@ -15,7 +17,9 @@ void deleteIrrelevantFields(string inString, string outString){
 	FILE *fileIn = fopen(inString.c_str(), "r");
 	FILE *fileOut = fopen(outString.c_str(), "w");
 
+	int i = 0;
 	while(!feof(fileIn)){
+		cout << i++ << endl;
 //		current = fgetc(fileIn);
 //		fputc(current, fileOut);
 	}
@@ -28,8 +32,23 @@ int main() {
 	cout << "!!!DataTeam project start!!!" << endl;
 
 //	Eliminando campos irrelevantes de set de entrenamiento
-	string trainFile = string("../files/train.csv");
-	deleteIrrelevantFields(trainFile, "../files/trainWithoutIrrelevantFields.csv");
+	string trainFileName = string("../DataTeam/files/train.csv");
+//	deleteIrrelevantFields(trainFile, "../Data/trainWithoutIrrelevantFields.csv");
+
+	ifstream trainFile(trainFileName.c_str(), ios::in);
+//	infile.open (trainFileName.c_str());
+	if (trainFile.is_open())
+	{
+		cout << "train.csv opened!" << endl;
+
+		while (trainFile.good())
+			cout << (char) trainFile.get();
+
+		trainFile.close();
+	} else
+	{
+		cout << "train.csv error when open file" << endl;
+	}
 
 	return 0;
 }
